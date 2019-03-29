c0193739504b29321df193686de16a6498ba5806
Vinicius Ruiz <viniciusruiiz>
2019-03-28 23:32:29 -0300

    socket init

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({"concord":"melhor que discord"});
+    //res.json({"concord":"melhor que discord"});
+    res.sendFile('C:/Users/Vinicius/Desktop/concord/src/index.html');
 });
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import http from 'http';
+import socket from 'socket.io';
 
 import index from './routes/index'
 
 const app = express();
+const server = http.Server(app);
+const io = socket(server);
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -14,8 +18,12 @@ app.use(logger('dev'));
 
 app.use('/', index)
 
-app.listen(3000, () => {
-    console.log('concord ta ligado! uhu');
+io.on("connection", (socket) => {
+    console.log("a user has been connected")  
+})
+
+server.listen(3000, () => {
+    console.log('concord runing at port *3000');
 });
 
 export default app;
